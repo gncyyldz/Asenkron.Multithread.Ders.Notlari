2068970c85fecd4c446b67a0ce9f36fc98f25ce5
Gençay Yıldız <gyildizmail@gmail.com>
2024-02-01 23:08:41 +0300

    Thread Synchronization | Non-Blocking Synchronization & volatile Keyword

---
+++ Ders5/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Run();
+    }
+    static int i;
+    private static void Run()
+    {
+        Thread thread1 = new(() =>
+        {
+            while (true) 
+                Volatile.Write(ref i, Volatile.Read(ref i) + 1); 
+        });
+        Thread thread2 = new(() =>
+        {
+            while (true)
+            {
+                Console.WriteLine(Volatile.Read(ref i));
+            }
+        });
+        Thread thread3 = new(() =>
+        {
+            while (true)
+                Volatile.Write(ref i, Volatile.Read(ref i) - 1);
+        });
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+
+
+    }
+}
