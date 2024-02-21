528f8eb8c80e3a67c050696414b5b1b25344b52c
Gençay Yıldız <gyildizmail@gmail.com>
2024-02-22 00:45:14 +0300

    Thread Synchronization | ReaderWriterLock & ReaderWriterLockSlim

---
+++ Ders8/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //5 adet reader thread oluşturuluyor...
+        for (int i = 0; i < 5; i++)
+            new Thread(Read).Start();
+        //2 adet writer thread oluşturuluyor...
+        for (int i = 0; i < 2; i++)
+            new Thread(Write).Start();
+    }
+    static ReaderWriterLockSlim readerWriterLockSlim = new();
+    static int counter = 0;
+    static void Read()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                readerWriterLockSlim.EnterReadLock();
+                Console.WriteLine($"R : Thread {Thread.CurrentThread.ManagedThreadId} is reading : {counter}");
+            }
+            finally
+            {
+                readerWriterLockSlim.ExitReadLock();
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    static void Write()
+    {
+
+        try
+        {
+            readerWriterLockSlim.EnterWriteLock();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"W : Thread {Thread.CurrentThread.ManagedThreadId} is writing : {++counter}");
+                Thread.Sleep(200);
+            }
+        }
+        finally
+        {
+            readerWriterLockSlim.ExitWriteLock();
+        }
+    }
+}
