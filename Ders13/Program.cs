93ff4deee5934706a1298e1940a18a2438fdc9ee
Gençay Yıldız <gyildizmail@gmail.com>
2024-03-27 20:43:59 +0300

    Timers | Multi-Threaded Timers & Single-Threaded Timers

---
+++ Ders13/Program.cs
@@ -0,0 +1,26 @@
+﻿#region System.Threading.Timer
+//Timer timer = new(state =>
+//{
+//    Console.WriteLine(state);
+//}, "Tick!", 200, Timeout.Infinite);
+
+//Thread.Sleep(1000);
+////timer.Change(0, 1500);
+
+//Console.Read();
+#endregion
+#region System.Timers.Timer
+System.Timers.Timer timer = new();
+timer.Elapsed += (sender, e) =>
+{
+    Console.WriteLine("asdlamsdk");
+};
+timer.Start();
+timer.Interval = 500;
+timer.Stop();
+
+Thread.Sleep(1000);
+//timer.Change(0, 1500);
+
+Console.Read();
+#endregion
