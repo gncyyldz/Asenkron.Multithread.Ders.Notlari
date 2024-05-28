804406cf65c7580609948bf8fffd3f65eab3289f
Gençay Yıldız <gyildizmail@gmail.com>
2024-05-28 22:53:44 +0300

    async&await Keyword’leri Eşliğinde Asenkron Programlama

---
+++ Ders16/Program.cs
@@ -0,0 +1,35 @@
+﻿//async void ReadFile()
+//{
+//    StreamReader streamReader = new("...");
+//    var task = streamReader.ReadToEndAsync();
+
+
+//    var content = await task;
+
+//    //Action action = async () =>
+//    //{
+
+//    //};
+
+//    return content;
+//}
+
+
+//await ReadFile();
+
+
+// Hata Durumlarına örnek
+async Task ReadFile()
+{
+    throw new Exception();
+}
+
+
+try
+{
+    await ReadFile();
+}
+catch (Exception ex)
+{
+
+}
