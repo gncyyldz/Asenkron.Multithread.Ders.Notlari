5479fb7eb9d7744f8a1bb6a783f420e26020ae86
Gençay Yıldız <gyildizmail@gmail.com>
2024-10-09 00:05:10 +0300

    Channels Library

---
+++ Ders20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+var channel = Channel.CreateBounded<int>(5);
+
+Task producer = Task.Run(async () =>
+{
+    for (int i = 0; i < 50; i++)
+    {
+        await channel.Writer.WriteAsync(i);
+        await Console.Out.WriteLineAsync($"Üretilen : {i}");
+        await Task.Delay(1000);
+    }
+
+    channel.Writer.Complete();
+});
+
+
+Task consumer = Task.Run(async () =>
+{
+    await foreach (var result in channel.Reader.ReadAllAsync())
+    {
+        await Console.Out.WriteLineAsync($"Tüketilen : {result}");
+        await Task.Delay(3000);
+    }
+});
+
+await Task.WhenAll(producer, consumer);
+
+Console.Read();
