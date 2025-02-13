b4d04082043d15d2d0ec8352eaab4358c67f0045
Gençay Yıldız <gyildizmail@gmail.com>
2025-02-13 22:10:45 +0300

    Fix

---
+++ Ders22/Program.cs
@@ -0,0 +1,12 @@
+﻿#region PLINQ - Ordering
+var numbers = Enumerable.Range(1, 999999999);
+var result = numbers
+    .AsParallel()
+    .AsOrdered()
+    .Where(n => n % 7777 == 0);
+
+foreach (var number in result)
+{
+    Console.WriteLine(number);
+}
+#endregion
