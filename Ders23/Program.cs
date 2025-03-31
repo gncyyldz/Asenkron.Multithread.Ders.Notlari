4046264826938780a735119f74c2707c1ee36c3d
Gençay Yıldız <gyildizmail@gmail.com>
2025-03-31 17:25:02 +0300

    Lesson 23

---
+++ Ders23/Program.cs
@@ -0,0 +1,21 @@
+﻿#region PLINQ & Cancellation
+var cancellationTokenSource = new CancellationTokenSource();
+
+var numbers = Enumerable.Range(0, 100000);
+var _numbers = numbers
+  .AsParallel()
+  .WithCancellation(cancellationTokenSource.Token)
+  .Where(n => n % 4 == 0)
+  .Select(n => n * n);
+
+Task.Run(() =>
+{
+    Thread.Sleep(1000);
+    cancellationTokenSource.Cancel();
+});
+
+foreach (var number in _numbers)
+    Console.WriteLine(number);
+
+Console.Read();
+#endregion
